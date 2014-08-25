8610ec19f15bf4dd8984b75ad2df035498eb1697
Dan <Salazar@nyu.edu>
2014-08-25 10:04:52 -0400

    added tagged lists for nouns and proper nouns

--- __init__.py
+++ __init__.py
@@ -1,10 +1,23 @@
 import nltk
 import re
 
-sentence='Bob. Adam. Washnigton. This is a sentence that I will be taking out the nouns from. Apple apple.';
+sentence='Bob. Adam. Washnigton. This %&( 9(((  is a sentence that I will be taking out the nouns from. Apple apple.';
 
 def NL(text):
-	split=text.split()
-	print nltk.pos_tag(split);
-print sentence
+	noun=[]
+	proper=[]
+	onlywords=re.findall(r'\b([a-zA-Z]+)\b',text)
+	tagged = nltk.pos_tag(onlywords)
+	for words in tagged:
+		if 'NN' in words:
+			noun.append(words);
+		elif 'NNS' in words:
+			noun.append(words);
+	for words in tagged:
+		if 'NNP' in words:
+			proper.append(words);
+		elif 'NNPS' in words:
+			proper.append(words);
+	print noun
+	print proper
 NL(sentence)
