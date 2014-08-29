ae9a06ee5bd7edf3dc973c185185aac3a96c88f1
Dan <Salazar@nyu.edu>
2014-08-29 13:54:51 -0400

    using list comprehension

--- __init__.py
+++ __init__.py
@@ -1,23 +1,19 @@
 import nltk
 import re
+from collections import Counter
 
 sentence='Bob. Adam. Washnigton. This %&( 9(((  is a sentence that I will be taking out the nouns from. Apple apple.';
 
 def NL(text):
-	noun=[]
-	proper=[]
-	onlywords=re.findall(r'\b([a-zA-Z]+)\b',text)
-	tagged = nltk.pos_tag(onlywords)
-	for words in tagged:
-		if 'NN' in words:
-			noun.append(words);
-		elif 'NNS' in words:
-			noun.append(words);
-	for words in tagged:
-		if 'NNP' in words:
-			proper.append(words);
-		elif 'NNPS' in words:
-			proper.append(words);
-	print noun
-	print proper
+	onlywords=re.findall(r'\b([a-zA-Z]+)\b',text);
+	tagged = nltk.pos_tag(onlywords);
+	nouns=[
+		words for words in tagged 
+		if 'NN' in words or 'NNS' in words]
+        proper_nouns=[
+		words for words in tagged 
+		if 'NNP' in words or 'NNPS' in words]
+	print nouns;
+	print proper_nouns;
+
 NL(sentence)
