872da4041f55f0920e02d3c01777b9edf16a70c2
Dan <Salazar@nyu.edu>
2014-09-03 19:05:10 -0400

    added lists for names, organizations, and locations

--- __init__.py
+++ __init__.py
@@ -2,18 +2,63 @@ import nltk
 import re
 from collections import Counter
 
-sentence='Bob. Adam. Washnigton. This %&( 9(((  is a sentence that I will be taking out the nouns from. Apple apple.';
-
 def NL(text):
-	onlywords=re.findall(r'\b([a-zA-Z]+)\b',text);
-	tagged = nltk.pos_tag(onlywords);
-	nouns=[
-		words for words in tagged 
-		if 'NN' in words or 'NNS' in words]
-        proper_nouns=[
-		words for words in tagged 
-		if 'NNP' in words or 'NNPS' in words]
-	print nouns;
-	print proper_nouns;
+        #leaves only letters from text
+        onlywords=re.findall(r'\b([a-zA-Z]+)\b',text);
+        
+        #tags words for parts of speech
+        tagged = nltk.pos_tag(onlywords);
+        
+        #extracts nouns from tagged words
+        nouns=[
+                words[0] for words in tagged
+                if 'NN' in words or 'NNS' in words]
+        freq_top_nouns=[
+                Counter(nouns).most_common(10)]
 
-NL(sentence)
+        #tags proper nouns from tagged words
+        pnouns=[
+                words[0] for words in tagged
+                if 'NNP' in words or 'NNPS' in words]
+        freq_top_pnouns=[
+                Counter(pnouns).most_common(10)]        
+        
+      
+        #extracts names from tagged words
+        raw_tags = ne_chunk(tagged)
+        raw_names=list(raw_tags.subtrees(filter=lambda x: x.node=='PERSON'))
+        names=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in raw_names if isinstance(ne, nltk.tree.Tree)]
+        freq_top_names=[Counter(names).most_common(10)]
+
+        
+        
+        organization_raw=list(raw_tags.subtrees(filter=lambda x: x.node=='ORGANIZATION'))
+        organization=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in organization_raw if isinstance(ne, nltk.tree.Tree)]
+        
+        location_raw=list(raw_tags.subtrees(filter=lambda x: x.node=='GPE'))
+        location=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in location_raw if isinstance(ne, nltk.tree.Tree)]
+
+        facility_raw=list(raw_tags.subtrees(filter=lambda x: x.node=='FACILITY'))
+        facility=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in facility_raw if isinstance(ne, nltk.tree.Tree)]
+        
+        verbs=[
+               words[0] for words in tagged
+               if 'VB' in words]
+        
+        print 'Nouns: '
+        print freq_top_nouns[0];
+        print 'Proper Nouns: '
+        print freq_top_pnouns[0];
+        print 'Names: '
+        print freq_top_names[0];
+        #print freq_top_names
+        #print top_names
+        print 'Organizations: '
+        print organization;
+        print 'Locations: '
+        print location;
+        print 'Facility: '
+        print facility;
+        
+NL(realsentence)
+
