37a38fce97b77e818b012d095bafbd68bb4203ae
Dan <Salazar@nyu.edu>
2014-09-18 22:04:48 -0700

    added class functionality

--- __init__.py
+++ __init__.py
@@ -1,64 +1,58 @@
+
+m nltk import pos_tag, ne_chunk
 import nltk
 import re
 from collections import Counter
 
-def NL(text):
-        #leaves only letters from text
-        onlywords=re.findall(r'\b([a-zA-Z]+)\b',text);
+class NL(object):
+    def __init__(self,text):
+        self.text=text
         
-        #tags words for parts of speech
-        tagged = nltk.pos_tag(onlywords);
+    def clean(self):
+        only_text=re.findall(r'\b([a-zA-Z]+)\b',self.text)
+        tagged=nltk.pos_tag(only_text)
+        return tagged
+    
+    def tagged_tree(self):
+        ctagged=ne_chunk(self.clean())
+        return ctagged
         
-        #extracts nouns from tagged words
+    def noun(self):
         nouns=[
-                words[0] for words in tagged
+                words[0] for words in self.clean()
                 if 'NN' in words or 'NNS' in words]
         freq_top_nouns=[
                 Counter(nouns).most_common(10)]
-
-        #tags proper nouns from tagged words
+        return freq_top_nouns[0]
+    
+    def pnoun(self):
         pnouns=[
-                words[0] for words in tagged
+                words[0] for words in self.clean()
                 if 'NNP' in words or 'NNPS' in words]
         freq_top_pnouns=[
-                Counter(pnouns).most_common(10)]        
-        
-      
-        #extracts names from tagged words
-        raw_tags = ne_chunk(tagged)
-        raw_names=list(raw_tags.subtrees(filter=lambda x: x.node=='PERSON'))
+                Counter(pnouns).most_common(10)]
+        return freq_top_pnouns[0]
+    
+    def person(self):
+        raw_names=list(self.tagged_tree().subtrees(filter=lambda x: x.node=='PERSON'))
         names=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in raw_names if isinstance(ne, nltk.tree.Tree)]
-        freq_top_names=[Counter(names).most_common(10)]
-
-        
-        
-        organization_raw=list(raw_tags.subtrees(filter=lambda x: x.node=='ORGANIZATION'))
+        top_persons=[Counter(names).most_common(10)]
+        return top_persons[0]
+    
+    def organization(self):
+        organization_raw=list(self.tagged_tree().subtrees(filter=lambda x: x.node=='ORGANIZATION'))
         organization=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in organization_raw if isinstance(ne, nltk.tree.Tree)]
-        
-        location_raw=list(raw_tags.subtrees(filter=lambda x: x.node=='GPE'))
+        top_organizations=[Counter(organization).most_common(10)]
+        return top_organizations[0]
+    
+    def location(self):
+        location_raw=list(self.tagged_tree().subtrees(filter=lambda x: x.node=='GPE'))
         location=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in location_raw if isinstance(ne, nltk.tree.Tree)]
-
-        facility_raw=list(raw_tags.subtrees(filter=lambda x: x.node=='FACILITY'))
+        top_locations=[Counter(location).most_common(10)]
+        return top_locations[0]
+    
+    def facility(self):
+        facility_raw=list(self.tagged_tree().subtrees(filter=lambda x: x.node=='FACILITY'))
         facility=[' '.join(map(lambda x: x[0], ne.leaves())) for ne in facility_raw if isinstance(ne, nltk.tree.Tree)]
-        
-        verbs=[
-               words[0] for words in tagged
-               if 'VB' in words]
-        
-        print 'Nouns: '
-        print freq_top_nouns[0];
-        print 'Proper Nouns: '
-        print freq_top_pnouns[0];
-        print 'Names: '
-        print freq_top_names[0];
-        #print freq_top_names
-        #print top_names
-        print 'Organizations: '
-        print organization;
-        print 'Locations: '
-        print location;
-        print 'Facility: '
-        print facility;
-        
-NL(realsentence)
-
+        top_facilities=[Counter(facility).most_common(10)]
+        return top_facilities[0]
